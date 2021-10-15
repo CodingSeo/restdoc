425b122aec0018232789d6b39552b2dcc2ee7d7e
Diego(서진교) <sjg@gabia.com>
2021-10-15 17:08:13 +0900

    - TestController Created

---
+++ initial/src/test/java/com/example/testingrestdocs/HomeControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.testingrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+//https://spring.io/guides/gs/testing-restdocs/#initial
+
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class WebLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
+    }
+}
