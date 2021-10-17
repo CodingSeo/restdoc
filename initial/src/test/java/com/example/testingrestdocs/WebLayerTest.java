0b3e3361160ebf52f5c3206adaa0d014f9811359
Diego(서진교) <sjg@gabia.com>
2021-10-18 08:18:01 +0900

    - HomeController Created - WebLayerTest Created

--- initial/src/test/java/com/example/testingrestdocs/WebLayerTest.java
+++ initial/src/test/java/com/example/testingrestdocs/WebLayerTest.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 
 //https://spring.io/guides/gs/testing-restdocs/#initial
 
-
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
